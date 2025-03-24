668f11b77581475c37c4ef15b7c7dc73e9e6c906
Yuta Osawa <yutaosawa81@gmail.com>
2025-03-24 20:00:08 +0900

    sign & verify

--- internal/keypair/p256.go
+++ internal/keypair/p256.go
@@ -44,8 +44,21 @@ func DecodeP256PrivateKey(der []byte) (*ecdsa.PrivateKey, error) {
 
 	key, ok := parsed.(*ecdsa.PrivateKey)
 	if !ok {
-		err = errors.New("not an ECDSA private key")
+		return nil, errors.New("not an ECDSA private key")
+	}
+
+	return key, nil
+}
+
+func DecodeP256PublicKey(der []byte) (*ecdsa.PublicKey, error) {
+	pub, err := x509.ParsePKIXPublicKey(der)
+	if err != nil {
 		return nil, err
+	}
+
+	key, ok := pub.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, errors.New("not an ECDSA public key")
 	}
 
 	return key, nil
@@ -56,4 +69,14 @@ func Fingerprint(der []byte) []byte {
 	h.Write(der)
 	fp := h.Sum(nil)
 	return fp
+}
+
+func Sign(priv *ecdsa.PrivateKey, message []byte) ([]byte, error) {
+	hash := sha256.Sum256(message)
+	return ecdsa.SignASN1(rand.Reader, priv, hash[:])
+}
+
+func Verify(pub *ecdsa.PublicKey, message, sig []byte) bool {
+	hash := sha256.Sum256(message)
+	return ecdsa.VerifyASN1(pub, hash[:], sig)
 }
